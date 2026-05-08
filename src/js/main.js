7ab38070fcdd9b93451cb9af5abb83258d31c9ec
pusteaghayeva <puste.aghayeva@gmail.com>
2026-05-08 15:02:31 +0400

    request

--- src/js/main.js
+++ src/js/main.js
@@ -173,41 +173,7 @@ $(document).ready(function () {
         }
     });
 });
-// single news
-// jQuery("#carousel").owlCarousel({
-//     autoplay: true,
-//     rewind: false,
-//     margin: 0,
-//     loop: true,
 
-//     animateOut: 'fadeOut',
-//     animateIn: 'fadeIn',
-//     responsiveClass: true,
-//     autoHeight: true,
-//     autoplayTimeout: 10000,
-//     smartSpeed: 2500,
-//     nav: true,
-//     navText: [
-//         '<svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path  fill="#306279" d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>',
-//         '<svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path  fill="#306279" d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>'],
-//     responsive: {
-//         0: {
-//             items: 1
-//         },
-
-//         600: {
-//             items: 1
-//         },
-
-//         1024: {
-//             items: 1
-//         },
-
-//         1366: {
-//             items: 1
-//         }
-//     }
-// });
 jQuery(document).ready(function() {
     var itemCount = jQuery("#carousel .item").length;
 
